bb61700975f89106615e86c41f35cde1048ce3cc
ulti72 <goole4money@gmail.com>
2018-05-28 11:45:18 +0530

    Basic Functionality Working

---
+++ app/src/main/java/ulti72/intime/MainActivity.java
@@ -0,0 +1,133 @@
+package ulti72.intime;
+
+import android.content.SharedPreferences;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private static  long START_TIME_IN_MILLIS = 86400000;
+    private static   long start ;
+
+    private TextView mTextViewCountDown;
+    private TextView mUsedTime;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis;
+    private long mEndTime;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewCountDown = (TextView)findViewById(R.id.text_view_countdown);
+        mUsedTime = (TextView)findViewById(R.id.used_time);
+
+
+       resetTimer();
+
+    }
+
+    private void start_point(){
+        Calendar c = Calendar.getInstance();
+        long now = c.getTimeInMillis();
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+         start = now - c.getTimeInMillis();
+
+    }
+    private void startTimer() {
+        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+            }
+        }.start();
+
+        mTimerRunning = true;
+    }
+
+
+
+    private void resetTimer() {
+        start_point();
+        mTimeLeftInMillis = START_TIME_IN_MILLIS-start;
+        startTimer();
+
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000);
+        int left = (int) 86400 -minutes ;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "$ %d", minutes);
+        String usedTime = String.format(Locale.getDefault(), "$ %d", left);
+
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+        mUsedTime.setText(usedTime);
+    }
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putLong("millisLeft", mTimeLeftInMillis);
+        editor.putBoolean("timerRunning", mTimerRunning);
+        editor.putLong("endTime", mEndTime);
+
+        editor.apply();
+
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+
+        mTimeLeftInMillis = prefs.getLong("millisLeft", START_TIME_IN_MILLIS);
+        mTimerRunning = prefs.getBoolean("timerRunning", false);
+
+        updateCountDownText();
+
+        if (mTimerRunning) {
+            mEndTime = prefs.getLong("endTime", 0);
+            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+
+            if (mTimeLeftInMillis < 0) {
+                mTimeLeftInMillis = 0;
+                mTimerRunning = false;
+                updateCountDownText();
+            } else {
+                startTimer();
+            }
+        }
+    }
+}
