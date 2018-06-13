9a7d127fbed200edc06b77a72253d1ee9b5628a7
ulti72 <goole4money@gmail.com>
2018-06-13 13:28:07 +0530

    Menu button addetd with working

--- app/src/main/java/ulti72/intime/MainActivity.java
+++ app/src/main/java/ulti72/intime/MainActivity.java
@@ -1,12 +1,14 @@
 package ulti72.intime;
 
+
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
 
 
 import java.util.Calendar;
@@ -34,6 +36,17 @@ public class MainActivity extends AppCompatActivity {
 
 
             resetTimer();
+
+        TextView menu = (TextView)findViewById(R.id.menu);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
+                resetTimer();
+            }
+        });
 
 
 
--- app/src/main/java/ulti72/intime/Menu.java
+++ app/src/main/java/ulti72/intime/Menu.java
@@ -1,7 +1,11 @@
 package ulti72.intime;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +13,47 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+        ImageView ra = (ImageView)findViewById(R.id.RA);
+        ra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.goole4moneygmail.napapp");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+        ImageView maimg = (ImageView)findViewById(R.id.imageView2);
+        maimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:abhishekpro47@gmail.com")); // only email apps should handle thi
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+
+            }
+        });
+        ImageView im =(ImageView) findViewById(R.id.share);
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/plain");
+                    i.putExtra(Intent.EXTRA_SUBJECT, "NapApp");
+                    String sAux = "\nLet me recommend you this application\n\n";
+                    sAux = sAux + "https://play.google.com/store/apps/details?id=com.goole4moneygmail.napapp \n\n";
+                    i.putExtra(Intent.EXTRA_TEXT, sAux);
+                    startActivity(Intent.createChooser(i, "choose one"));
+                } catch(Exception e) {
+                    //e.toString();
+                }
+            }
+        });
+
     }
 }
